033332bde59f231885ec84bec23b0711acac74e8
Patryk Miler <patryk.miler@student.pk.edu.pl>
2024-11-17 16:13:05 +0100

    *css refactoring

--- code/scripts/mobile_meu.js
+++ code/scripts/mobile_meu.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".menu_show");
 const menu = document.querySelector("nav  ul");
-const mobile_only = document.querySelectorAll(".mobile_only");
+const mobile_only = document.querySelectorAll(".mobile_only_option");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
