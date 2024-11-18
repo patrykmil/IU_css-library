f1741ce4dab110708b4c5fb56edd16fbff626834
Patryk Miler <patryk.miler@student.pk.edu.pl>
2024-11-18 15:23:12 +0100

    added login page

--- index.php
+++ index.php
@@ -6,9 +6,12 @@ $path = $_SERVER['REQUEST_URI'];
 
 var_dump($path);
 
-if ($path == '/index') {
+if ($path == '/component_page') {
   $controller = new AppController();
-  $controller->render('index');
+  $controller->render('component_page');
+} else if ($path == '/login_page') {
+  $controller = new AppController();
+  $controller->render('login_page');
 } else {
   $controller = new AppController();
   $controller->render('index');
