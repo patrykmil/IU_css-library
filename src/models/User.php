9d84ec633cad7ee02cf6116a6acd74405d1d52e9
Patryk Miler <patryk.miler@student.pk.edu.pl>
2024-12-09 19:58:24 +0100

    autorization v1

---
+++ src/models/User.php
@@ -0,0 +1,51 @@
+<?php
+
+class User {
+    private string $email;
+    private string $nickname;
+    private string $password;
+
+    /**
+     * @param string $email
+     * @param string $password
+     * @param string $nickname
+     */
+    public function __construct(string $email, string $nickname, string $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->nickname = $nickname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getNickname(): string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): void
+    {
+        $this->nickname = $nickname;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+
+}
