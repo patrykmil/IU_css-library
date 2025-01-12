7c15da7d3c292e3507cdeee5218c4d074e32ac4c
Patryk Miler <patryk.miler@student.pk.edu.pl>
2025-01-12 15:34:18 +0100

    +component examples

--- database_schema/data.sql
+++ database_schema/data.sql
@@ -23,61 +23,44 @@ INSERT INTO public."Avatar" (avatarid, avatarpath) VALUES (9, 'hair_purple.svg')
 
 -- User inserts
 INSERT INTO public."User" (userid, email, nickname, passwordhash, avatarid, isadmin) VALUES (1, 'iuadmin@iu.iu', 'admin1', '$2y$12$9cqkVFs2HxdhzN0ZPp8/1uudWjvDZT5YJVI4euVIUJjgPazullhtm', 3, true);
-INSERT INTO public."User" (userid, email, nickname, passwordhash, avatarid) VALUES (2, 'patryk@gmail.com', 'patryk', '$2a$12$dpeQ2M1m0kCa.QcLL8CTtespIjB2Do29Vih3X3QOjadM1jBmUDdWe', 1);
-INSERT INTO public."User" (userid, email, nickname, passwordhash, avatarid) VALUES (3, 'none@proton.me', 'none', '$2a$12$tHTVAWSwNysA8WW3gXGd6uBebCJFzDD/0r2NkO54vGUqlncxDXzz.', 8);
+INSERT INTO public."User" (userid, email, nickname, passwordhash, avatarid) VALUES (2, 'patryk@gmail.com', 'patryk', '$2a$12$AO9m5bhM66mJ6VYJmpcnDeyiB14EmM1FyDhP5plk6wAvSwD0YEirm', 1);
+INSERT INTO public."User" (userid, email, nickname, passwordhash, avatarid) VALUES (3, 'none@proton.me', 'none', '$2y$12$d/MJQW2V0jQkYbLLJqZRpOHyH8Fcs4Bqh78i17oqhAASJjPEInV5K', 8);
 
 --Type inserts
 INSERT INTO public."Type" (typeid, name) VALUES (1, 'button');
 INSERT INTO public."Type" (typeid, name) VALUES (2, 'input');
+INSERT INTO public."Type" (typeid, name) VALUES (3, 'checkbox');
+INSERT INTO public."Type" (typeid, name) VALUES (4, 'radio button');
 
 --Color inserts
 INSERT INTO public."Color" (colorid, hex) VALUES (1, '00F0FF');
 INSERT INTO public."Color" (colorid, hex) VALUES (2, '13A823');
 INSERT INTO public."Color" (colorid, hex) VALUES (3, 'FFFFFF');
-
---Set inserts
-INSERT INTO public."Set" (setid, name, ownerid) VALUES (1, 'neon', 1);
-INSERT INTO public."Set" (setid, name, ownerid) VALUES (2, 'basic', 2);
-INSERT INTO public."Set" (setid, name, ownerid) VALUES (3, 'modern', 3);
-INSERT INTO public."Set" (setid, name, ownerid) VALUES (4, 'pastel', 2);
+INSERT INTO public."Color" (colorid, hex) VALUES (4, 'FF5733');
+INSERT INTO public."Color" (colorid, hex) VALUES (5, 'C70039');
+INSERT INTO public."Color" (colorid, hex) VALUES (6, '900C3F');
+INSERT INTO public."Color" (colorid, hex) VALUES (7, '581845');
+INSERT INTO public."Color" (colorid, hex) VALUES (8, 'DAF7A6');
 
 --Tag inserts
 INSERT INTO public."Tag" (tagid, name, colorid) VALUES (1, 'simple', 1);
 INSERT INTO public."Tag" (tagid, name, colorid) VALUES (2, 'fancy', 2);
 INSERT INTO public."Tag" (tagid, name, colorid) VALUES (3, 'colorless', 3);
-
---Component inserts
-INSERT INTO public."Component" (componentid, name, setid, typeid, colorid, css, html, authorid, createdat) VALUES (1, 'button1', 1, 1, 1, 'button {background-color: red;}', '&lt;button&gt;Click me&lt;/button&gt;', 1, '2021-01-01 00:00:00');
-INSERT INTO public."Component" (componentid, name, setid, typeid, colorid, css, html, authorid, createdat) VALUES (2, 'button2', 1, 1, 2, 'button {background-color: green;}', '&lt;button&gt;Click me&lt;/button&gt;', 1, '2021-01-01 00:00:00');
-INSERT INTO public."Component" (componentid, name, setid, typeid, colorid, css, html, authorid, createdat) VALUES (3, 'input1', 3, 2, 3, 'input {background-color: blue;}', '&lt;input&gt;', 1, '2021-01-01 00:00:00');
-INSERT INTO public."Component" (componentid, name, setid, typeid, colorid, css, html, authorid, createdat) VALUES (4, 'button3', 2, 1, 1, 'button {background-color: red;}', '&lt;button&gt;Click me&lt;/button&gt;', 2, '2021-01-01 00:00:00');
-
---ComponentTag inserts
-INSERT INTO public."ComponentTag" (componentid, tagid) VALUES (1, 1);
-INSERT INTO public."ComponentTag" (componentid, tagid) VALUES (1, 3);
-INSERT INTO public."ComponentTag" (componentid, tagid) VALUES (2, 2);
-INSERT INTO public."ComponentTag" (componentid, tagid) VALUES (3, 3);
-INSERT INTO public."ComponentTag" (componentid, tagid) VALUES (4, 1);
-INSERT INTO public."ComponentTag" (componentid, tagid) VALUES (4, 2);
-
---Likes inserts
-INSERT INTO public."Likes" (componentid, userid) VALUES (1, 2);
-INSERT INTO public."Likes" (componentid, userid) VALUES (1, 3);
-INSERT INTO public."Likes" (componentid, userid) VALUES (2, 2);
-INSERT INTO public."Likes" (componentid, userid) VALUES (3, 3);
-INSERT INTO public."Likes" (componentid, userid) VALUES (4, 2);
-
-
---Bookmark inserts
-INSERT INTO public."Bookmark" (bookmarkid, userid, name) VALUES (1, 2, 'To remember!');
-INSERT INTO public."Bookmark" (bookmarkid, userid, name) VALUES (2, 3, 'Saved for later');
-INSERT INTO public."Bookmark" (bookmarkid, userid, name) VALUES (3, 2, 'For work project');
-
---BookmarkComponent inserts
-INSERT INTO public."BookmarkComponent" (bookmarkid, componentid) VALUES (1, 1);
-INSERT INTO public."BookmarkComponent" (bookmarkid, componentid) VALUES (1, 2);
-INSERT INTO public."BookmarkComponent" (bookmarkid, componentid) VALUES (2, 3);
-INSERT INTO public."BookmarkComponent" (bookmarkid, componentid) VALUES (3, 4);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (4, 'responsive', 4);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (5, 'animated', 5);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (6, 'modest', 6);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (7, 'modern', 7);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (8, 'classic', 8);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (9, 'dark', 1);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (10, 'light', 2);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (11, 'flat', 3);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (12, 'gradient', 4);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (13, 'shadow', 5);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (14, 'bordered', 6);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (15, 'rounded', 7);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (16, 'outlined', 8);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (17, 'transparent', 1);
+INSERT INTO public."Tag" (tagid, name, colorid) VALUES (18, 'solid', 2);
 
 --Reset sequence
 SELECT setval(pg_get_serial_sequence('"User"', 'userid'), coalesce(max(userid)+1, 1), false) FROM "User";
